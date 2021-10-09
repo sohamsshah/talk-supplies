b87e651d43dd0efa5f0ca8ae6822c0d10a5e68f9
sohamsshah <sohamshah456@gmail.com>
2021-10-09 10:45:00 +0530

    test

---
+++ jsmeetupblr/react-snowpack/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
+if (undefined /* [snowpack] import.meta.hot */ ) {
+undefined /* [snowpack] import.meta.hot */ .accept();
+}
